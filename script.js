b983bbefa7c2177d285f2ddc6ca84a9cd6dec9d5
Yashada18 <yashadajagtap18@gmail.com>
2025-07-06 09:01:03 +0530

    Initial commit on Trial branch with updated homepage and assets

---
+++ script.js
@@ -0,0 +1,5 @@
+tsParticles.load("tsparticles", {
+  background:{color:"#0d0d0d"},
+  particles:{number:{value:80},size:{value:2},color:{value:"#00ffcc"},move:{enable:true,speed:1},opacity:{value:0.3},links:{enable:true,distance:100,color:"#00ffcc",opacity:0.3}},
+  fullScreen:{enable:false}
+});
